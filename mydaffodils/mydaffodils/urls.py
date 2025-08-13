6d1b199f05839e93523d15b04c160a9f85ef8af0
Laxmiptl830 <lp6104319@gmail.com>
2025-08-13 10:09:17 +0530

    Initial commit

--- mydaffodils/mydaffodils/urls.py
+++ mydaffodils/mydaffodils/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for wedding project.
+URL configuration for mydaffodils project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
